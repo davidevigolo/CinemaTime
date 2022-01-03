f11243c3e1bb5eb1d7a1727cd60f19f7066fa1e1
BuildTools <unconfigured@null.spigotmc.org>
2022-01-03 14:35:28 +0100

    Aggiungere i file di progetto.

---
+++ BotPollo/AttributesModels/Command.cs
@@ -0,0 +1,49 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotPollo.Attributes
+{
+    class Command : Attribute
+    {
+        public Command(string Name)
+        {
+            this.Name = Name;
+        }
+        public string Name { get; set; }
+    }
+
+    class Setup
+    {
+        private static Dictionary<string, MethodInfo> CommandMap = new Dictionary<string, MethodInfo>();
+        public static void RegisterCommands(Object obj)
+        {
+            var types = from t in obj.GetType().GetMethods() where t.GetCustomAttributes<Command>().Count() > 0 select t;
+
+            foreach (MethodInfo mi in types)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("[Logging][Internal]" + DateTime.Now.ToString(" HH:mm:ss") + " Info     Command: " + ((Command)mi.GetCustomAttribute(typeof(Command))).Name + " binded to method: " + mi.Name);
+                CommandMap.Add(((Command)mi.GetCustomAttribute(typeof(Command))).Name.ToLower(), mi);
+            }
+        }
+
+        internal static Task Command_Handler(SocketMessage msg)
+        {
+            if (CommandMap.ContainsKey(msg.Content.ToLower()))
+            {
+                var method = CommandMap.GetValueOrDefault(msg.Content.ToLower());
+                method.Invoke(null, new object[] { msg });
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("[Logging][Internal]" + DateTime.Now.ToString(" HH:mm:ss") + " Info     User: " + msg.Author.Username + " Used command: " + ((Command)method.GetCustomAttribute(typeof(Command))).Name.ToLower());
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ BotPollo/Core/Commands.cs
@@ -0,0 +1,38 @@
+﻿using BotPollo.Attributes;
+using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotPollo.Core
+{
+    class Commands
+    {
+        [Command("checkpunti")]
+        public static async Task CheckPunti(SocketMessage msg)
+        {
+            var channel = msg.Channel;
+            await channel.SendMessageAsync("troppi troppi");
+        }
+
+        [Command("invito")]
+        public static async Task InviteMessage(SocketMessage msg)
+        {
+            var channel = msg.Channel;
+            Discord.EmbedBuilder builder = new Discord.EmbedBuilder()
+            {
+                Title = "Click here to add the bot to your server",
+                Description = "https://discord.com/api/oauth2/authorize?client_id=885174574591930448&permissions=8&scope=bot",
+                Color = Discord.Color.Blue,
+                ThumbnailUrl = "https://cdn.discordapp.com/avatars/885174574591930448/e840f87959f3b280e335d78e02263cfa.webp?size=128"
+            };
+
+            await channel.SendMessageAsync("", false, builder.Build());
+        }
+
+
+    }
+}
